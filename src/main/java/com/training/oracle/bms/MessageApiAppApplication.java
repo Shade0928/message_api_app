cd5928945547269c94b6ceb55ccf9afe21ee7a51
Anant Raj <mr.shade0928@gmail.com>
2024-07-12 14:26:20 +0530

    first commit

---
+++ src/main/java/com/training/oracle/bms/MessageApiAppApplication.java
@@ -0,0 +1,18 @@
+package com.training.oracle.bms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageApiAppApplication
+{
+public static void greetings()
+{
+	System.out.println("Hi Oracle");
+}
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MessageApiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/training/oracle/bms/MessageApiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.oracle.bms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageApiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
