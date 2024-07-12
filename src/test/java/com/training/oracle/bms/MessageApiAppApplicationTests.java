b863199257f37b17335a9ac2973be42aed1b9c10
Anant Raj <mr.shade0928@gmail.com>
2024-07-12 14:37:41 +0530

    To new beginnings

--- src/test/java/com/training/oracle/bms/MessageApiAppApplicationTests.java
+++ src/test/java/com/training/oracle/bms/MessageApiAppApplicationTests.java
@@ -3,11 +3,14 @@ package com.training.oracle.bms;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class MessageApiAppApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() 
+	{
+		assertEquals(200,190);
 	}
 
 }
