f69ba2b6a95f244c0cc18ffd7fedfe4889926cf7
Anant Raj <mr.shade0928@gmail.com>
2024-07-12 15:12:14 +0530

    To new beginnings again

--- src/main/java/com/training/oracle/bms/MessageApiAppApplication.java
+++ src/main/java/com/training/oracle/bms/MessageApiAppApplication.java
@@ -2,6 +2,7 @@ package com.training.oracle.bms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class MessageApiAppApplication
@@ -9,6 +10,11 @@ public class MessageApiAppApplication
 public static void greetings()
 {
 	System.out.println("Hi Oracle");
+}
+@GetMapping("day")
+public String getday()
+{
+	return ("Today is Friday");
 }
 	public static void main(String[] args)
 	{
